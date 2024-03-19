e901f81e3ef6f89f53ef6c1f2ff0fb86eb0da0f0
Nguyễn Hồng Vân <hongvan.bbx.ozanni@gmail.com>
2024-03-19 10:34:33 +0700

    test jenkins 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { initializeDayjs } from './utils/initializeDayjs';
 initializeDayjs();
 
 function App() {
-  console.log('test jenkins');
+  console.log('test jenkins 2');
   return (
     <AppProvider store={store}>
       <AppRoutes />
